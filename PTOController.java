01a6f3880ef5ef43b47d5e6e5bdc9583569069d6
maclainja <63621911+maclainja@users.noreply.github.com>
2020-04-16 17:27:52 -0500

    Wrote PTODatabase and PTOController

---
+++ PTOController.java
@@ -0,0 +1,42 @@
+
+
+public class PTOController {
+
+   PTODatabase PTODatabase;
+
+   public PTOController() {
+      PTODatabase = new PTODatabase();
+   }
+
+   public int getNewID(String type) {
+      return PTODatabase.getNewID(type);
+   }
+
+   public boolean verifyPTO(double amount, int employeeID) {
+      return PTODatabase.verifyPTO(amount, employeeID);
+   }
+
+   public void addPTO(double amount, int employeeID) {
+      PTODatabase.addPTO(amount, employeeID);
+   }
+   
+   public void deductPTO(double amount, int employeeID) {
+      PTODatabase.deductPTO(amount, employeeID);
+   }
+   
+   public double getPTO(int employeeID) {
+      return PTODatabase.getPTO(employeeID);
+   }
+
+   public void donatePTO(double amountRequested, int giverID, int receiverID) {
+      PTODatabase.getPTO(giverID);
+      PTODatabase.getPTO(receiverID);
+      if (PTODatabase.verifyPTO(amountRequested, giverID)) {
+         PTODatabase.deductPTO(amountRequested, receiverID);
+         PTODatabase.addPTO(amountRequested, receiverID);
+         return;
+      }
+      throw new IllegalArgumentException("Giver employee does not have the required PTO");
+   }
+
+}
