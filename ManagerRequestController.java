95ff3b98062cb7b3f438b943e5b6368973f298eb
alc0091 <53951274+alc0091@users.noreply.github.com>
2020-04-16 23:14:54 -0500

    Update ManagerRequestController.java

--- ManagerRequestController.java
+++ ManagerRequestController.java
@@ -1,29 +1,19 @@
 
 public class ManagerRequestController {
 
-   PersonController pc;
-   RequestController rc;
+   RequestDatabaseController rdb;
 
    public ManagerRequestController() {
-      pc = new PersonController();
-      rc = new RequestController();
+      rdb = new RequestDatabaseController();
    }
 
-   // public void approveRequest() {
-   // 
-   // }
-// 
-   // public void rejectRequest() {
-   // 
-   // }
-   
-   public void setStatus(Request newRequest, Gender fm, Title jobTitle)
-   {
-      Request newRequest = rc.setStatus(newRequest, fm, jobTitle)
-      
-      Gender fm = pc.getGender(newRequest.empployeeID);
-      Title jobTitle = pc.getTitle(newRequest.employeeID);
-   
+   public void approveRequest() {
+    // rdb.approveRequest()
    }
+ 
+   public void rejectRequest() {
+    // rdb.rejectRequest()
+   }
+ 
 
 }
